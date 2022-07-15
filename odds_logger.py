09c052298fcc5ff21525810dce6f6f46d4ce1990
cjarmstrong2018 <christian.armstrong25@gmail.com>
2022-07-14 22:45:46 -0500

    added arb sniffing functionality for odds_logger.py

--- odds_logger.py
+++ odds_logger.py
@@ -36,11 +36,10 @@ class OddsLogger(object):
             'oddsFormat': 'american'
         }
         odds_req = requests.get(base_url + odds_endpoint, params=params)
-        print(odds_req.headers['x-requests-remaining'])
         odds = json.loads(odds_req.text)
         odds = odds['data']
         self.games = []
-
+        no_arb = True
         for game in odds:
             if dt.fromtimestamp(game['commence_time']) < dt.now():  # Ignore live odds
                 continue
@@ -55,10 +54,13 @@ class OddsLogger(object):
                 game['sites'], home_first=home_first, draw_possible=False)
             books_quoting = [s['site_key'] for s in game['sites']]
 
-            # Input arb funcs here!
             if self.arb_exists(odds_by_sb):
                 # Calculate surebet
-                continue
+                msg_dict = beat_bookies(row['Best Home Odds'], row['Home'], row['Best Book Home'],
+                                        row['Best Odds Away'], row['Away'], row['Best Book Away'])
+                alert = self.format_alert(msg_dict)
+                DiscordAlert(alert)
+                no_arb = False
 
             for book in SportsBooks:
                 if book.name in books_quoting:
@@ -86,10 +88,10 @@ class OddsLogger(object):
                             (not currently supported) 
         '''
         odds = {}
-        odds['Best Home Book'] = None
-        odds['Best Away Book'] = None
-        odds['Best Home Odds'] = -10000
-        odds['Bet Away Odds'] = -1000
+        odds['Best Book Home'] = None
+        odds['Best Book Away'] = None
+        odds['Best Odds Home'] = -10000
+        odds['Best Odds Away'] = -10000
         for site in sites:
             name = site['site_key']
             last_update = site['last_update']
@@ -104,7 +106,7 @@ class OddsLogger(object):
             odds['Best Odds Home'] = home_odds if home_odds > odds['Best Odds Home'] else odds['Best Odds Home']
             odds['Best Odds Away'] = away_odds if away_odds > odds['Best Odds Away'] else odds['Best Odds Away']
             odds['Best Book Home'] = name if home_odds > odds['Best Odds Home'] else odds['Best Book Home']
-            odds['Best Book Away'] = name if away_odds > odds['Best Away Odds'] else odds['Best Book Away']
+            odds['Best Book Away'] = name if away_odds > odds['Best Odds Away'] else odds['Best Book Away']
             odds[name + '_home'] = home_odds
             odds[name + '_away'] = away_odds
             odds[name + '_last_update'] = last_update
@@ -141,12 +143,42 @@ class OddsLogger(object):
         best_odds = [odds_by_sb[x]
                      for x in odds_by_sb.keys() if "Best Odds" in x]
         for odds in best_odds:
-            d_odds = OddsLogger.decimal_odds(odds)
+            d_odds = decimal_odds(odds)
             sure_bet += 1 / d_odds
         if sure_bet < 1:
             return True
         else:
             return False
+
+    def format_msg(self, msg_dict):
+        """
+        Formats Message to be passed to DiscordAlert for notification
+        Args:
+            msg_dict (dict): Dictionary from beat_bookies
+        """
+        heading = "ALERT: Arb Spotted! ACT FAST" + "\n"
+        intro = f"For a total stake of {msg_dict['Total Stake']} place the following bets: \n \n"
+        bets = f"{msg_dict['Home Book']}: {msg_dict['Home Team']} ML at {msg_dict['Home Odds']} for {msg_dict['Home Stake']} \n" + \
+            f"{msg_dict['Away Book']}: {msg_dict['Away Team']} ML at {msg_dict['Away Odds']} for {msg_dict['Away Stake']} \n"
+        profit = f"This will result in a profit of {msg_dict['Home Profit']} or {msg_dict['Away Profit']}"
+        return heading + intro + bets + profit
+
+
+def decimal_odds(odds: int) -> float:
+    """
+    :param odds: Integer (e.g., -350).
+    :return: Float. Odds expressed in Decimal terms.
+    """
+    if isinstance(odds, float):
+        return odds
+
+    elif isinstance(odds, int):
+        if odds >= 100:
+            return abs(1 + (odds / 100))
+        elif odds <= -101:
+            return 100 / abs(odds) + 1
+        else:
+            return float(odds)
 
 
 def beat_bookies(home_odds, home_team, home_book, away_odds, away_team, away_book, total_stake):
@@ -159,26 +191,24 @@ def beat_bookies(home_odds, home_team, home_book, away_odds, away_team, away_boo
     profit2 = away_odds*stakes[y] - total_stake
     benefit1 = f'{profit1 / total_investment * 100:.2f}%'
     benefit2 = f'{profit2 / total_investment * 100:.2f}%'
-    dict_gabmling = {'Odds1': home_odds, 'Odds2': away_odds, 'Home Stake': f'${stakes[x]:.0f}', 'Away Stake': f'${stakes[y]:.0f}', 'Home Profit': f'${profit1:.2f}', 'Away Profit': f'${profit2:.2f}',
-                     'Benefit1': benefit1, 'Benefit2': benefit2, "Home Book": home_book, "Home Team": home_team, 'Away Book': away_book, 'Away Team': away_team}
+    dict_gabmling = {'Home Odds': home_odds, 'Away Odds': away_odds, 'Home Stake': f'${stakes[x]:.0f}', 'Away Stake': f'${stakes[y]:.0f}', 'Home Profit': f'${profit1:.2f}', 'Away Profit': f'${profit2:.2f}',
+                     'Benefit1': benefit1, 'Benefit2': benefit2, "Home Book": home_book, "Home Team": home_team, 'Away Book': away_book, 'Away Team': away_team, "Total Stake": total_stake}
     return dict_gabmling
 
-    @staticmethod
-    def decimal_odds(odds: int) -> float:
-        """
-        :param odds: Integer (e.g., -350).
-        :return: Float. Odds expressed in Decimal terms.
-        """
-        if isinstance(odds, float):
-            return odds
 
-        elif isinstance(odds, int):
-            if odds >= 100:
-                return abs(1 + (odds / 100))
-            elif odds <= -101:
-                return 100 / abs(odds) + 1
-            else:
-                return float(odds)
+class DiscordAlert(object):
+    """
+    Class to send a message to my discord bot
+    Args:
+        msg (str):message to send to discord channel 
+    Returns:
+        nothing, posts message to channel
+    """
+
+    def __init__(self, msg) -> None:
+        self.url = "https://discord.com/api/webhooks/997338106258796674/m7M3DKqc12-cNphOw6qJMeWdZzo7wkDjXp1fwZihSlOV2HsQ8NkWBrR2XU25QL9h3Z8n"
+        data = {'content': msg}
+        r = requests.post(url=self.url, json=data)
 
 
 if __name__ == '__main__':
